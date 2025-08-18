aa74ac1789e95b979d18f59f8714a3713f7d0c9f
Sunil Upadhyay <sunil.aryal@gmail.com>
2025-08-18 08:26:30 -0400

    solution bounce.py

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,11 @@
 # bounce.py
 #
 # Exercise 1.5
+
+init_height = 100.0 # in meters
+interim_height = init_height
+
+for i in range(10):
+    bounce_height = (3/5) * interim_height
+    print(i, round(bounce_height, ndigits=4))
+    interim_height = bounce_height
